e20591c9319b97b6b88a860cd55bcd23bcb97146
Pierre <pgadea7@gmail.com>
2017-04-06 05:02:04 -0500

    Eager Loading Entities

---
+++ ComicBookGalleryModel/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        public Context()
+        {
+            Database.SetInitializer(new DatabaseInitializer());
+        }
+        public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5, 2);
+        }
+    }
+}
--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using ComicBookGalleryModel.Models;
+using System.Data.Entity;
+using System.Diagnostics;
 
 namespace ComicBookGalleryModel
 {
@@ -10,6 +14,66 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.Database.Log = (message) => Debug.WriteLine(message);
+
+               //var comicBooks = context.ComicBooks.ToList();
+
+                //var comicBooksQuery = from cb in context.ComicBooks select cb;
+
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .OrderByDescending(cb => cb.IssueNumber)
+                //    .ThenBy(cb => cb.PublishedOn)
+                //    .ToList();
+
+                var comicBooksQuery = context.ComicBooks
+                    .Include(cb => cb.Series)
+                    .OrderByDescending(cb => cb.IssueNumber);
+
+                var comicBooks = comicBooksQuery.ToList();
+
+                var comicBooks2 = comicBooksQuery
+                    .Where(cb => cb.AverageRating < 7)
+                    .ToList();
+
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks.Count);
+                Console.WriteLine();
+
+
+                foreach (var comicBook in comicBooks2)
+                {
+                    Console.WriteLine(comicBook.DisplayText);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("# of comic books: {0}", comicBooks2.Count);
+
+
+
+                //var comicBooks = context.ComicBooks
+                //    .Include(cb => cb.Series)
+                //    .Include(cb => cb.Artists.Select(a => a.Artist))
+                //    .Include(cb => cb.Artists.Select(a => a.Role))
+                //    .ToList();
+
+                //foreach (var comicBook in comicBooks)
+                //{
+                //    var artistRoleNames = comicBook.Artists
+                //        .Select(a => $"{a.Artist.Name} - {a.Role.Name}").ToList();
+                //    var artistRolesDisplayText = string.Join(", ", artistRoleNames);
+
+                //    Console.WriteLine(comicBook.DisplayText);
+                //    Console.WriteLine(artistRolesDisplayText);
+                //}
+                Console.ReadLine();
+            }
         }
     }
 }
